d8b1188de85174d3373d4ac5937a5bb7e62e6019
Rohit Negi <rohit.506n@gmail.com>
2026-05-01 00:24:37 +0530

    cors fix 2

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,6 @@ const corsOptions = {
 export const app = express();
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json({ limit: "8mb" }));
 
 app.get("/health", (_req, res) => {
